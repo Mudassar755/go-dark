86e4dae3de5738d0e3c7ecfe27828511a2bc8a52
Mudassar755 <mudasar.se@gmail.com>
2021-09-23 14:53:39 +0500

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import PropTypes from "prop-types";
+import store, { persistor } from "../store/store";
+import "../styles/styles.scss";
+import Layout from "../components/layout/Layout";
+
+// fonts
+import "../fonts/styles.css";
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {/* <Layout> */}
+        <Component {...pageProps} />
+        {/* </Layout> */}
+      </PersistGate>
+    </Provider>
+  );
+};
+
+App.propTypes = {
+  Component: PropTypes.func,
+  pageProps: PropTypes.object,
+};
+
+export default App;
